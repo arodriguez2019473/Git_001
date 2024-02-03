6203177d7df8a2bfe1785ba2150034046d754ee9
arodriguez2019473 <arodriguez-2019473@kinal.edu.gt>
2024-02-02 18:00:10 -0600

    se hace el boton de usuario y el de favoritos

--- assets/scripts/Logica_Contactos.js
+++ assets/scripts/Logica_Contactos.js
@@ -2,6 +2,14 @@ function agregarContacto() {
     alert('Vamos a nuevo contacto');
     window.location.href = '../Paginas/nuevo_Contactos.html';
 }
+function Favoritos() {
+    window.location.href = '../Paginas/Favorito.html';
+  }
+
+  function Usuario() {
+   window.location.href = '../Paginas/Favorito.html';
+ }
+
 
 function mostrarInformacion(infoId) {
    var infoDetallada = document.getElementById(infoId);
